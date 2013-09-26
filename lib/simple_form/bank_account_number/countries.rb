e453b1c250e262671cf58a1cec670373192e50f8
Cédric Félizard <cedric@felizard.fr>
2013-09-26 22:53:49 +0000

    Update formats to include edge cases

--- lib/simple_form/bank_account_number/countries.rb
+++ lib/simple_form/bank_account_number/countries.rb
@@ -3,16 +3,15 @@ module SimpleForm
     COUNTRIES = {
       "AU" => [
         { label: "6 digit BSB number", format: /\d{6}/ },
-        { label: "9 digit account number", format: /\d{9}/ },
+        { label: "6 to 9 digit account number", format: /\d{6,9}/ },
       ],
       "CA" => [
-        { label: "5 digit branch number", format: /\d{5}/ },
-        { label: "3 digit institution number", format: /\d{3}/ },
-        { label: "account number", format: /\d+/ },
+        { label: "8 or 9 digit transit number", format: /\d{8,9}/ },
+        { label: "7 to 12 digit account number", format: /\d{7,12}/ },
       ],
       "GB" => [
         { label: "6 digit sort code", format: /\d{6}/ },
-        { label: "8 digit account number", format: /\d{8}/ },
+        { label: "7 to 10 digit account number", format: /\d{7,10}/ },
       ],
       "NZ" => [
         { label: "2 digit bank number", format: /\d{2}/ },
